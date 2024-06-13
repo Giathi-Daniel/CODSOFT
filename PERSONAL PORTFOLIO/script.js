8e3cbdf0cb35cf9b6ff1996baa4b83dbf9452131
Daniel-Giathi <giathidaniel252@gmail.com>
2024-06-13 11:44:52 +0300

    Add navigation

--- PERSONAL PORTFOLIO/script.js
+++ PERSONAL PORTFOLIO/script.js
@@ -1,35 +1,31 @@
-document.getElementById('downloadButton').addEventListener('click', function() {
-    // Set the source of the <embed> element to display the PDF
-    let pdfEmbed = document.getElementById('pdfEmbed');
-    pdfEmbed.src = 'My Resume.pdf';
-    pdfEmbed.style.display = 'block';
-
-    // Download the PDF file
-    let url = 'My Resume.pdf';
-    let filename = 'My Resume.pdf'; 
-
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.responseType = 'blob';
-
-    xhr.onload = function() {
-        let blob = new Blob([xhr.response], {type: 'application/octet-stream'});
-        let link = document.createElement('a');
-        link.href = window.URL.createObjectURL(blob);
-        link.download = filename;
-
-        document.body.appendChild(link);
-
-        link.click();
-
-        document.body.removeChild(link);
-    };
-
-    xhr.send();
-});
+// window onscroll
+const navigation = document.querySelector('nav');
+window.addEventListener("scroll", () => {
+  if(this.scrollY > 200) {
+    navigation.classList.add('active');
+  } else {
+    navigation.classList.remove('active')
+  }
+})
 
 
-// SLIDES
+// NAVIGATION BAR
+const menuBtn = document.querySelector('#menuOpen');
+const navItems = document.querySelector('.nav__list');
+const menuClose = document.querySelector('#menuClose');
+
+function toggleMenu() {
+  navItems.classList.toggle('active');
+  menuClose.style.display = (navItems.classList.contains('active')) ? 'inline-block' : 'none';
+  menuBtn.style.display = (navItems.classList.contains('active')) ? 'none' : 'inline-block';
+}
+
+menuBtn.addEventListener('click', toggleMenu);
+menuClose.addEventListener('click', toggleMenu);
+
+
+
+// swiperJS
 var swiper = new Swiper(".mySwiper", {
   pagination: {
     el: ".swiper-pagination",
@@ -40,4 +36,21 @@ var swiper = new Swiper(".mySwiper", {
     delay: 5000,
     disableOnInteraction: false,
   }
+});
+
+
+// download button
+document.getElementById('downloadButton').addEventListener('click', function() {
+  let pdfEmbed = document.getElementById('pdfEmbed');
+  pdfEmbed.src = 'My Resume.pdf';
+  pdfEmbed.style.display = 'block';
+
+  fetch('My Resume.pdf')
+      .then(response => response.blob())
+      .then(blob => {
+          let link = document.createElement('a');
+          link.href = window.URL.createObjectURL(blob);
+          link.download = 'My Resume.pdf';
+          link.click();
+      });
 });
