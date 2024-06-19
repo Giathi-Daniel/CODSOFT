c848d98293ffc4a9bd3803fcc537454725b9fa46
Daniel-Giathi <giathidaniel252@gmail.com>
2024-06-19 20:04:43 +0300

    Add js setup

---
+++ FreshiLandingPage/script.js
@@ -0,0 +1,42 @@
+// SWIPER
+// var swiper = new Swiper('.product-slider', {
+//     loop:true,
+//     spaceBetween: 20,
+//     autoplay: {
+//         delay: 7500,
+//         disableOnInteraction: false,
+//     },
+//     centeredSlides: true,
+//     breakpoints: {
+//       0: {
+//         slidesPerView: 2,
+//       },
+//       768: {
+//         slidesPerView: 3,
+//       },
+//       1020: {
+//         slidesPerView: 4,
+//       },
+//     },
+// });
+
+// var swiper = new Swiper(".review-slider", {
+//     loop:true,
+//     spaceBetween: 20,
+//     autoplay: {
+//         delay: 7500,
+//         disableOnInteraction: false,
+//     },
+//     centeredSlides: true,
+//     breakpoints: {
+//       0: {
+//         slidesPerView: 1,
+//       },
+//       768: {
+//         slidesPerView: 2,
+//       },
+//       1020: {
+//         slidesPerView: 3,
+//       },
+//     },
+//   });
