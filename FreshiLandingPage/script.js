5727ba3e470dbe2b24ae7f9f4164e3e0564ff93f
Daniel-Giathi <giathidaniel252@gmail.com>
2024-06-21 16:12:43 +0300

    Update header section

--- FreshiLandingPage/script.js
+++ FreshiLandingPage/script.js
@@ -1,3 +1,50 @@
+let search = document.querySelector('.search-form')
+let shoppingCart = document.querySelector('.shopping-cart')
+let loginForm = document.querySelector('.login-form')
+let formSubmit = document.querySelector('.toggle-form')
+let menuBtn = document.querySelector('.navbar')
+
+document.querySelector('#search-btn').onclick = () => {
+    search.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active')
+    menuBtn.classList.remove('active')
+}
+
+document.querySelector('#cart-btn').onclick = () => {
+    shoppingCart.classList.toggle('active');
+    search.classList.remove('active');
+    loginForm.classList.remove('active')
+    menuBtn.classList.remove('active')
+}
+
+
+formSubmit.addEventListener("click", () => {
+    const signupForm = document.querySelector('.signup-form')
+    signupForm.innerHTML += ``
+})
+
+document.querySelector('#login-btn').onclick = () => {
+    loginForm.classList.toggle('active')
+    shoppingCart.classList.remove('active');
+    search.classList.remove('active');
+    menuBtn.classList.remove('active')
+}
+
+document.querySelector('#menu-btn').onclick = () => {
+    menuBtn.classList.toggle('active')
+    loginForm.classList.remove('active')
+    shoppingCart.classList.remove('active')
+    search.classList.remove('active')
+}
+
+// remove active classes when scrolling
+window.onscroll = () => {
+    search.classList.remove('active')
+    shoppingCart.classList.remove('active')
+    loginForm.classList.remove('active')
+}
+
 // SWIPER
 // var swiper = new Swiper('.product-slider', {
 //     loop:true,
