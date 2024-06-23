de471a6c6ae5678aec530772247853103ea94e97
Daniel Giathi <blacksharkchi@proton.me>
2024-06-23 21:46:17 +0300

    Update index file

--- FreshiLandingPage/script.js
+++ FreshiLandingPage/script.js
@@ -46,45 +46,25 @@ window.onscroll = () => {
 }
 
 // SWIPER
-// var swiper = new Swiper('.product-slider', {
-//     loop:true,
-//     spaceBetween: 30,
-//     autoplay: {
-//         delay: 7500,
-//         disableOnInteraction: false,
-//     },
-//     centeredSlides: true,
-//     breakpoints: {
-//       0: {
-//         slidesPerView: 2,
-//       },
-//       768: {
-//         slidesPerView: 3,
-//       },
-//       1020: {
-//         slidesPerView: 4,
-//       },
-//     },
-// });
+var swiper = new Swiper(".swiper", {
+    slidesPerView: 1,
 
+    loop:true,
+    spaceBetween: 30,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
 
-// var swiper = new Swiper(".review-slider", {
-//     loop:true,
-//     spaceBetween: 20,
-//     autoplay: {
-//         delay: 7500,
-//         disableOnInteraction: false,
-//     },
-//     centeredSlides: true,
-//     breakpoints: {
-//       0: {
-//         slidesPerView: 1,
-//       },
-//       768: {
-//         slidesPerView: 2,
-//       },
-//       1020: {
-//         slidesPerView: 3,
-//       },
-//     },
-//   });
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      600: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+  });
